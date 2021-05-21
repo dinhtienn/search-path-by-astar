28141635ee76188aabaef9088fa59a86186f3cde
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2021-05-21 12:54:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,45 +2,130 @@
 let createGraph = require('ngraph.graph');
 let graph = createGraph();
 
-const listLocation = ['BẠCH MAI', 'THANH NHÀN', 'NGÕ QUỲNH', 'KIM NGƯU', 'TRẦN KHÁT CHÂN', 'VÕ THỊ SÁU'];
+const listLocation = {
+    'BẠCH MAI': ['A', 'B', 'C'], 
+    'THANH NHÀN': ['C', 'D', 'E', 'F'], 
+    'NGÕ QUỲNH': ['B', 'D'], 
+    'KIM NGƯU': ['G', 'F'], 
+    'TRẦN KHÁT CHÂN': ['A', 'H', 'G'], 
+    'VÕ THỊ SÁU': ['H', 'E'],
+    'LẠC TRUNG': ['F'],
+};
 
-graph.addNode('BẠCH MAI', {x: 21.008728, y: 105.851407});
-graph.addNode('Thanh Nhàn', {x: 21.003149, y: 105.850914});
-graph.addNode('NGÕ QUỲNH', {x: 21.005623, y: 105.851150});
-graph.addNode('KIM NGƯU', {x: 21.003089, y: 105.861536});
-graph.addNode('TRẦN KHÁT CHÂN', {x: 21.009068, y: 105.860631});
-graph.addNode('VÕ THỊ SÁU', {x: 21.008954, y: 105.854686});
+const listNode = {
+    'A': {x: 21.008703, y: 105.851438},
+    'B': {x: 21.005588, y: 105.851164},
+    'C': {x: 21.003134, y: 105.850917},
+    'D': {x: 21.002856, y: 105.854357},
+    'E': {x: 21.002816, y: 105.854956},
+    'F': {x: 21.003034, y: 105.861303},
+    'G': {x: 21.009054, y: 105.860635},
+    'H': {x: 21.008953, y: 105.854680},
+}
 
-graph.addLink('BẠCH MAI', 'NGÕ QUỲNH');
-graph.addLink('BẠCH MAI', 'VÕ THỊ SÁU');
-graph.addLink('NGÕ QUỲNH', 'Thanh Nhàn');
-graph.addLink('KIM NGƯU', 'Thanh Nhàn');
-graph.addLink('KIM NGƯU', 'TRẦN KHÁT CHÂN');
-graph.addLink('VÕ THỊ SÁU', 'TRẦN KHÁT CHÂN');
-graph.addLink('VÕ THỊ SÁU', 'Thanh Nhàn', {x: 21.002838, y: 105.854950});
+for (const [key, value] of Object.entries(listNode)) {
+    graph.addNode(key, value);
+}
+
+graph.addLink('A', 'B');
+graph.addLink('A', 'H');
+graph.addLink('B', 'C');
+graph.addLink('B', 'D');
+graph.addLink('C', 'D');
+graph.addLink('D', 'E');
+graph.addLink('E', 'F');
+graph.addLink('E', 'H');
+graph.addLink('F', 'G');
+graph.addLink('G', 'H');
+
+// Function
+distance = (fromNode, toNode) => {
+    let dx = fromNode.x - toNode.x;
+    let dy = fromNode.y - toNode.y;
+
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
+totalDistance = (listNodePath) => {
+    let total = 0;
+    for (let i = 0; i < listNodePath.length - 1; i++) {
+        const fromNode = listNodePath[i];
+        const toNode = listNodePath[i + 1];
+        total += distance(listNode[fromNode], listNode[toNode]);
+    }
+    return total;
+}
+
+checkNodeInLocation = (node) => {
+    let listLocationContain = [];
+    for (const [key, value] of Object.entries(listLocation)) {
+        if (value.includes(node)) listLocationContain.push(key);
+    }
+    return listLocationContain;
+}
+
+getLocationBetweenNode = (listLocation1, listLocation2) => {
+    for (let i = 0; i < listLocation1.length; i++) {
+        if (listLocation2.includes(listLocation1[i])) return listLocation1[i];
+    }
+    return 'NOT FOUND';
+}
 
 // Path
 let path = require('ngraph.path');
 let pathFinder = path.aStar(graph, {
     distance(fromNode, toNode) {
-      let dx = fromNode.data.x - toNode.data.x;
-      let dy = fromNode.data.y - toNode.data.y;
-
-      return Math.sqrt(dx * dx + dy * dy);
+        let dx = fromNode.data.x - toNode.data.x;
+        let dy = fromNode.data.y - toNode.data.y;
+    
+        return Math.sqrt(dx * dx + dy * dy);
     }
 });
 
+// Input data
 let from = '', to = '';
 const prompt = require('prompt-sync')();
  
-while (!listLocation.includes(from)) {
+while (!(from in listLocation)) {
     from = prompt('Nhập điểm xuất phát: ').toUpperCase();
-    if (!listLocation.includes(from)) console.log('Điểm xuất phát không có trên bản đồ');
+    if (!(from in listLocation)) console.log('Điểm xuất phát không có trên bản đồ');
 }
-while (!listLocation.includes(to)) {
+while (!(to in listLocation)) {
     to = prompt('Nhập điểm đến: ').toUpperCase();
-    if (!listLocation.includes(to)) console.log('Điểm đến không có trên bản đồ');
+    if (!(to in listLocation)) console.log('Điểm đến không có trên bản đồ');
 }
 
-let route = pathFinder.find(from, to);
-console.log('Đường đi: ' + route.reverse().map(item => item.id).join(' -> '));
+// Process data
+let listRoute = [];
+let listDistance = [];
+for (let i = 0; i < listLocation[from].length; i++) {
+    for (let j = 0; j < listLocation[to].length; j++) {
+        const route = pathFinder.find(listLocation[from][i], listLocation[to][j]);
+        listRoute.push(route);
+        listDistance.push(totalDistance(route.map(item => item.id)));
+    }
+}
+
+// Get shortest route
+let minDistance = listDistance[0];
+for (let i = 0; i < listDistance.length; i++) {
+    if (listDistance[i] < minDistance) minDistance = listDistance[i];
+}
+const index = listDistance.indexOf(minDistance);
+const resultPath = listRoute[index].reverse().map(item => item.id);
+
+// Process result
+let resultData = [];
+resultData.push(from);
+for (let i = 0; i < resultPath.length; i++) {
+    const listLocationContain = checkNodeInLocation(resultPath[i]);
+    if (listLocationContain.includes(to)) resultData.push(to);
+    else {
+        const location = getLocationBetweenNode(listLocationContain, checkNodeInLocation(resultPath[i + 1]));
+        if (resultData.includes(location)) continue;
+        resultData.push(location);
+    }
+}
+
+// Display result
+console.log('Đường đi đề xuất: ' + resultData.join(' -> '));
